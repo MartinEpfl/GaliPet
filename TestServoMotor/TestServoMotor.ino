0760906e6e62a55cc236e8056003618bfbf455d4
Unknown <Martin.Cibils@epfl.ch>
2020-10-24 18:28:38 +0200

    Some changes to Servo

--- TestServoMotor/TestServoMotor.ino
+++ TestServoMotor/TestServoMotor.ino
@@ -1,28 +1,47 @@
 
 #include <Servo.h>
-#include <Keyboard.h>
-#include <KeyboardController.h>
+//#include <KeyboardController.h>
 //USBHost : KeyboardController class
 
 Servo monServomoteur;
-USBHost usb;
-KeyboardController keyboard(usb);
-int position;
+int positionOfArm;
+byte incomingByte;
+
+
 void setup() {
   Serial.begin(9600);
   // Attache le servomoteur à la broche D9
 
-  monServomoteur.attach(9);
-  position = monServomoteur.read();
-
+  monServomoteur.attach(7);
+  positionOfArm = monServomoteur.read();
+  Serial.print(positionOfArm);
 }
 
 void loop() {
   
-  usb.Task();
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingByte = Serial.read();
+   
+    // say what you got:
+    Serial.print("I received: ");
+    Serial.println(incomingByte, DEC);
+    if(incomingByte==32){
+      Serial.print("Turning...");
 
-  
+      for (int position = 0; position <= 180; position++) {
+        monServomoteur.write(position);
+        delay(8);
+      }   
+      delay(1000);
+      for (int position = 180; position >= 0; position--) {
+        monServomoteur.write(position);
+        delay(8);
+      }    
+    }     
+  }
   /*
+  
   for (int position = 0; position <= 180; position++) {
     monServomoteur.write(position);
     delay(15);
@@ -34,15 +53,4 @@ void loop() {
     monServomoteur.write(position);
     delay(15);
   }*/
-}
-
-void keyPressed() {
-  Serial.print("Pressed:  ");
-  Serial.print(keyboard.getKey());
-  if(keyboard.getKey() == 'd' and position<180)
-    position++;
-  if(keyboard.getKey() == 'f' and position>0)
-    position--;  
-  monServomoteur.write(position);
-
 }
