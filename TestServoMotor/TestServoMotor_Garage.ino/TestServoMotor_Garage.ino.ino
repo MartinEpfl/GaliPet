88f2e9800cb8b0f1a9d77ec1cc49bc97110cc188
Max <rombach.maxime@gmail.com>
2020-11-05 09:15:42 +0100

    Moteur garage

---
+++ TestServoMotor/TestServoMotor_Garage.ino/TestServoMotor_Garage.ino.ino
@@ -0,0 +1,81 @@
+#include <Servo.h>
+//#include <KeyboardController.h>
+//USBHost : KeyboardController class
+
+Servo monServomoteur;
+int positionOfArm;
+int uplim;
+int lowlim;
+int upspeed;
+int downspeed;
+int setspeed;
+byte incomingByte;
+
+
+void setup() {
+  Serial.begin(9600);
+  // Attache le servomoteur à la broche D9
+
+  monServomoteur.attach(7);
+  positionOfArm = monServomoteur.read();
+  Serial.print(positionOfArm);
+
+  uplim = 70;
+  lowlim = 0;
+  setspeed = 10;
+  upspeed = 8;
+  downspeed = 8;
+
+  for (int positionA = positionOfArm; positionA <= uplim; positionA++) {
+        monServomoteur.write(positionA);
+        Serial.println(monServomoteur.read());
+        Serial.println("Init");
+        delay(setspeed);
+      } 
+}
+
+void loop() {
+
+ 
+ // if (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingByte = Serial.read();
+   
+    // say what you got:
+    //Serial.print("I received: ");
+    //monServomoteur.write(0);
+   // Serial.println(incomingByte, DEC);
+    Serial.println(monServomoteur.read());
+    //if(incomingByte==32){
+      Serial.print("Turning...");
+      for (int position = uplim; position > lowlim; position--) {
+        monServomoteur.write(position);
+        Serial.println("Clockwise");
+        Serial.println(position);
+
+       delay(upspeed);
+     // }   
+   }      
+      delay(1000);
+
+   for (int position = lowlim; position < uplim; position++) {
+        monServomoteur.write(position);
+               Serial.println("Anti-clockwise");
+
+        delay(downspeed);
+      }   
+   
+  }
+  /*
+  
+  for (int position = 0; position <= 180; position++) {
+    monServomoteur.write(position);
+    delay(15);
+  }
+  //Fait bouger le bras de 0° à 180°
+
+  // Fait bouger le bras de 180° à 10°
+  for (int position = 180; position >= 0; position--) {
+    monServomoteur.write(position);
+    delay(15);
+  }*/
