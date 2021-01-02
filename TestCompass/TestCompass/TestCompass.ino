554bbb39ba224eeaa7c2af0f54f4353098f890ff
Max <rombach.maxime@gmail.com>
2021-01-02 18:02:36 +0100

    basic compass again

--- TestCompass/TestCompass/TestCompass.ino
+++ TestCompass/TestCompass/TestCompass.ino
@@ -3,10 +3,6 @@
   Criado por Igor Araujo - www.igoraraujo.eng.br - 2012
 */
 
-#include <SharpIR.h>
-#include <Servo.h>
-
-
 #define SWIRPin A4
 #define model 1080
 
@@ -17,74 +13,26 @@ byte valor = 0;
 double diff = 0;
 double angle = 0;
 double distanceNE;
-SharpIR sensorNW = SharpIR(SWIRPin, model);
-
-Servo servoArm;
-int pinservoArm =  9; //Pin of servo for the ARM (PWM)
-int positionOfArm;
-int uplim = 500; //Position when the arm is at the top
-int lowlim = 2700; //Position when the arm is at the bottom
-int upspeedFirstPart = 3;
-int upspeedSecondPart = 3;
-int downspeed = 1;
-int setspeed = 10;
-int waitingOnBottleTime = 1000; //Time waited on the bottle
-int intermediatePosition = 1000;
-
-
-Servo servoBack;
-int pinServoBack = 8; //Pin of servo for the back (PWM)
-int positionOfBack;
-int uplim_b = 110; //Position of back when close
-int lowlim_b = 0; //Position of back when open
-int speedBack = 25; //Speed back is opening/closing
-int waitingBottleOut = 3000; //Waiting for bottle to go out
-
-
-int E1 = 6;     //M1 Speed Control (PWM)
-int M1 = 27;     //M1 Direction Control (Digital)
-int E2 = 7; //M2 Speed Control (PWM)
-int M2 = 29; //M2 Direction control (Digital)
-int speed_ = 125; //Speed moving backward cm/s
 
 void setup() {
   //  servoArm.attach(pinservoArm);
   //servoArm.write(20);
   //servoBack.attach(pinServoBack);
 
-  Serial.begin(9600);
+  Serial.begin(19200);
   Serial2.begin(9600);
-  pinMode(E1, OUTPUT);
-  pinMode(M1, OUTPUT);
-  pinMode(E2, OUTPUT);
-  pinMode(M2, OUTPUT);
-
+  
   readValueCompass();
 
   diff = PI / 4 - angle;
 }
 
 void loop() {
-  distanceNE = sensorNW.distance();
+
   readValueCompass();
   //Serial.println(angle);
-  if (Serial.available()) {
-    char val = Serial.read();
-    if (val != -1)
-    {
-      switch (val)
-      {
-        case 'w'://Move Forward
-          // Serial.println("Move forward");
-          advance (speed_, speed_);   //move forward in max speed
-          break;
-        case 'x'://Move Backward
-          //Serial.println("STOP");
-          stop();   //move back in max speed
-          break;
-      }
-    }
-  }
+  delay(10);
+ 
 }
 
 void readValueCompass() {
@@ -109,24 +57,4 @@ void readValueCompass() {
 
   //        Serial.print("THIS IS THE ANGLE FROM THE COMPASS VALUE : ");
   Serial.println(tempAngleCompass);
-}
-
-void stop(void)  //Stop
-{
-  digitalWrite(M1, LOW);
-  digitalWrite(M2, LOW);
-  digitalWrite(E1, 0);
-  digitalWrite(E2, 0);
-}
-
-
-void advance(char a, char b)  //Move forward
-{
-  digitalWrite(M1, HIGH);
-  digitalWrite(M2, LOW);
-
-  analogWrite (E1, a);     //PWM Speed Control
-  analogWrite (E2, b);
-
-
 }
